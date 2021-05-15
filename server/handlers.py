4c0cf6b34f2b13f59b97973cb815f5aebe43b48a
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2021-05-15 21:22:03 +0200

    Add specific quote and sentiment handler implementations

--- server/handlers.py
+++ server/handlers.py
@@ -3,6 +3,7 @@ from http.server import BaseHTTPRequestHandler
 import json
 import re
 import requests
+import time
 
 from config import cfg
 
@@ -35,6 +36,7 @@ class BaseHandler(ABC):
 
 
 class QuotesHandler(BaseHandler):
+
     number_query_pattern = re.compile(r'^number=(?P<number>[0-9]{1,2})$')
     min_quotes = int(cfg["SERVER"]["MIN_QUOTES"])
     max_quotes = int(cfg["SERVER"]["MAX_QUOTES"])
@@ -51,8 +53,18 @@ class QuotesHandler(BaseHandler):
         return True
 
     def get_response(self) -> dict:
+        quotes = set()
+        while len(quotes) < self.quote_n:
+            try:
+                response = requests.get(cfg["SERVER"]["KANYE_HOST"])
+            except requests.exceptions.RequestException as e:
+                self.handler.send_error(500, 'Kanye API may have some problems...')
+            quotes.add(response.json()["quote"])
+            # Let's be polite :)
+            time.sleep(float(cfg["SERVER"]["KANYE_REQUEST_RATE"]))
+
         return {
-            "quotes": ["quote1", "quote2"]
+            "quotes": list(quotes)
         }
 
 
@@ -62,20 +74,60 @@ class SentimentHandler(BaseHandler):
         # Is content type JSON?
         if self.handler.headers["Content-Type"] != "application/json":
             return False
+
         # Is there any content at all?
         if not self.handler.headers["Content-Length"]:
             return False
         content_length = int(self.handler.headers["Content-Length"])
         post_data = self.handler.rfile.read(content_length)
+
         # Is json valid?
         try:
             self.post_data = json.loads(post_data.decode("utf-8"))
         except ValueError:
             return False
+
         # Is there only a single field
         if not self.post_data.get("quotes") or len(self.post_data) != 1:
+            return False
+
+        # Quotes need to be a list
+        if not isinstance(self.post_data["quotes"], list):
             return False
         return True
 
     def get_response(self) -> dict:
-        return self.post_data
+        quotes = self.post_data["quotes"]
+        quote_polarity = {}
+        for quote in quotes:
+            try:
+                response = requests.post(
+                    cfg["SERVER"]["SENTIM_HOST"],
+                    json={"text": quote},
+                )
+            except requests.exceptions.RequestException as e:
+                self.handler.send_error(500, ' Sentim API may have some problems...')
+            quote_polarity[quote] = float(response.json()["result"]["polarity"])
+            # Let's be polite :)
+            time.sleep(float(cfg["SERVER"]["SENTIM_REQUEST_RATE"]))
+        return self.construct_counts_response(quote_polarity)
+
+    def construct_counts_response(self, quote_polarity):
+        response = {
+            "positive_count": 0,
+            "negative_count": 0,
+            "neutral_count": 0,
+            "most_extreme": None,
+        }
+        most_extreme_polarity = 0
+        for quote, polarity in quote_polarity.items():
+            if polarity > 0:
+                response["positive_count"] += 1
+            elif polarity < 0:
+                response["negative_count"] += 1
+            else:
+                response["neutral_count"] += 1
+            if abs(polarity) > most_extreme_polarity:
+                response["most_extreme"] = quote
+                most_extreme_polarity = abs(polarity)
+        return response
